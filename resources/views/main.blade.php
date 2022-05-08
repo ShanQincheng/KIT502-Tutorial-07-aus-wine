b77aafbdebd5f41c78c0ef8a75c93eb2585f7df8
shanqincheng <codingstory@outlook.com>
2022-05-09 03:32:44 +1000

    images

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <h1>This is our main page</h1>
+        <h1>KIT502 Self Study for Laravel</h1>
+
+        <div class="row">
+            <div class="col-lg-4">
+                <img class="img-responsive" src="{{url('../images/stationery_green.jpg')}}"
+                     alt="what would our like to use as an value for this attribute?">
+            </div>
+            <div class="col-lg-4">
+                <img class="img-responsive" src="{{url('../images/stationery.jpg')}}"
+                     alt="this is image of stationery">
+            </div>
+            <div class="col-lg-4">
+                <img class="img-responsive" src="{{url('../images/note_pencil.jpg')}}"
+                     alt="guess what">
+            </div>
+        </div>
     </div>
+
 @endsection
