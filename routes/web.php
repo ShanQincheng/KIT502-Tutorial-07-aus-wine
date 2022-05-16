6cfec5b9e78d3ceea467ee87aa53e9b87dd25d89
shanqincheng <codingstory@outlook.com>
2022-05-16 17:37:51 +1000

    delete useless code

--- routes/web.php
+++ routes/web.php
@@ -20,30 +20,6 @@ Route::get('/', function () {
     return view('main');
 }) -> name('home');
 
-Route::get('/users', function() {
-    return 'Welcome to the users page';
-});
-
-Route::get('/array', function() {
-   return ['KIT502', 'Web Development', 'Laravel'];
-});
-
-Route::get('/jsonObject', function() {
-   return response() -> json([
-      'unit' => 'KIT502',
-      'name' => 'Web Development',
-      'topic' => 'Laravel'
-   ]);
-});
-
-Route::get('/products', [ProductsController::class, 'index']) -> name('products');
-Route::get('/products/{name}',
-    [ProductsController::class, 'show']) -> where([
-        'name' => '[a-zA-Z]+',
-    ]);
-
-Route::get('about', [MainPageController::class, 'about']) -> name('about');
-
 Route::get('/wines', [WinesController::class, 'index']) -> name('wines');
 Route::post('/wines', [WinesController::class, 'store']) -> name('store.wines');
 Route::post('/wines/{ID}', [WinesController::class, 'edit']) -> name('edit.wines');
